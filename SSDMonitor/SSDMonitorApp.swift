7f306853b4cc8a842d4d2c29207be56aad407670
Paulo Hernane <paulohernane10@gmail.com>
2026-08-17 01:49:14 -0300

    Initial Commit

---
+++ SSDMonitor/SSDMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSDMonitorApp.swift
+//  SSDMonitor
+//
+//  Created by Paulo Hernane Fontes e Silva on 17/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SSDMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SSDMonitorTests/SSDMonitorTests.swift
@@ -0,0 +1,19 @@
+//
+//  SSDMonitorTests.swift
+//  SSDMonitorTests
+//
+//  Created by Paulo Hernane Fontes e Silva on 17/08/26.
+//
+
+import Testing
+@testable import SSDMonitor
+
+struct SSDMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
